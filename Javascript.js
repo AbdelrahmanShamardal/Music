196d81e10d830bad1bef5ee4a46f72f6f37713fe
AbdelrahmanShamardal <63594642+AbdelrahmanShamardal@users.noreply.github.com>
2021-01-02 01:28:24 +0200

    Update Javascript.js

--- Javascript.js
+++ Javascript.js
@@ -1,1 +1,9 @@
-window.addEventListener()
+document.addEventListener("click", myFunction);
+document.addEventListener("click", someOtherFunction);
+
+function myFunction() {
+  alert ("Hello World!");}
+
+
+function someOtherFunction() {
+  alert ("This function was also executed!");}
